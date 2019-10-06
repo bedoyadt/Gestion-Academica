8565c71cef868c791e72cce86cb2a1d02a3c3df8
yeison bedoya <bedoyadt@gmail.com>
2019-10-06 11:36:57 -0500

    terminando de A reglar Errores

--- src/index.js
+++ src/index.js
@@ -16,14 +16,14 @@ const app = express();
 require('./lib/passport');
 
 // Settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 2000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs',
-    helpers: require('./lib/handlebars')
+    extname: '.hbs'
+        //helpers: require('./lib/handlebars')
 }))
 app.set('view engine', '.hbs');
 
